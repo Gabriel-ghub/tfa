1c5ae59c16dd038e3028703d29db9354a70dc404
Gabriel-hub <romerogabriel.ad@gmail.com>
2022-04-14 00:25:42 +0200

    cambios en bdd

--- config.php
+++ config.php
@@ -4,9 +4,9 @@
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASS","");
-define("DB_NAME","tfa");
+define("DB_NAME","proyecto_daw011");
 
 //DEFINE URL
 
-define("ROOT_PATH","/Project_MVC/");
-define("ROOT_URL","http://localhost:8080/Project_MVC/");
+define("ROOT_PATH","/Proyecto_TF/");
+define("ROOT_URL","http://localhost:8080/Proyecto_TF/");
