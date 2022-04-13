d7f4c9c1c124118f90f60467dc0274702c28bb4f
Gabriel-hub <romerogabriel.ad@gmail.com>
2022-04-13 23:38:38 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+//DEFINE DB PARAMS
+
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","tfa");
+
+//DEFINE URL
+
+define("ROOT_PATH","/Project_MVC/");
+define("ROOT_URL","http://localhost:8080/Project_MVC/");
